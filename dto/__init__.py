3e9d79ce262e3a9ec607df84022506742eb60083
andriy898 <105068614+andriy898@users.noreply.github.com>
2026-05-20 13:00:07 +0300

    Update __init__.py

--- dto/__init__.py
+++ dto/__init__.py
@@ -1,1 +1,8 @@
+def user_dto(user):
+    return {"id": user.id, "username": user.username, "email": user.email, "city_id": user.city_id}
 
+def city_dto(city):
+    return {"id": city.id, "name": city.name}
+
+def hobby_dto(hobby):
+    return {"id": hobby.id, "name": hobby.name}
