006de3a80da94095110422704b801a5d1c338dc4
andriy898 <105068614+andriy898@users.noreply.github.com>
2026-05-20 13:48:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/lab_db'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1234@localhost/ldb_4'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
